584a71b167020525f59e70d4580fad886266940d
unknown <zlovred12@gmail.com>
2023-01-23 20:12:18 +0300

    fillDiagonal

--- src/main/java/ru/imbabot/lesson2/Main.java
+++ src/main/java/ru/imbabot/lesson2/Main.java
@@ -1,20 +1,46 @@
 package ru.imbabot.lesson2;
 
+
 import java.util.Arrays;
 
 public class Main {
 
-    private static void fillArr(int[] arr){
+    private static void fillDiagonal(int[][] arr) {
+        int first = 0;
+        int second = arr.length;
+
+        while (first < arr.length) {
+            arr[first][first] = 1;
+            first++;
+        }
+
+        while (first < arr.length) {
+            arr[first][second] = 1;
+            first++;
+            second--;
+        }
+    }
+
+
+    private static void lessThenSix(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+    }
+
+    private static void fillArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i * 3;
         }
     }
 
-    private static void changeNumbers(int[] arr){
+    private static void changeNumbers(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0){
+            if (arr[i] == 0) {
                 arr[i] = 1;
-            }else if (arr[i] == 1){
+            } else if (arr[i] == 1) {
                 arr[i] = 0;
             }
         }
@@ -25,10 +51,18 @@ public class Main {
 //        changeNumbers(arr);
 //        System.out.println(Arrays.toString(arr));
 //
-        int[] arr = new int[8];
-        fillArr(arr);
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = new int[8];
+//        fillArr(arr);
+//        System.out.println(Arrays.toString(arr));
+//
+//        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+//        lessThenSix(arr);
+//        System.out.println(Arrays.toString(arr));
 
+        int[][] arr = new int[5][5];
+        fillDiagonal(arr);
+
+        System.out.println(Arrays.deepToString(arr));
 
 
     }
